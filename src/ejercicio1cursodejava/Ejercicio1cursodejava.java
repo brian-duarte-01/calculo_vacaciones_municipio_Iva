5b9a506d3fb13066af450c79adf3976bd918ea13
brian0106 <56491425+brian0106@users.noreply.github.com>
2021-04-20 15:28:19 -0600

    version 1.0

---
+++ src/ejercicio1cursodejava/Ejercicio1cursodejava.java
@@ -0,0 +1,23 @@
+package ejercicio1cursodejava;
+
+public class Ejercicio1cursodejava {
+    
+    public static void main(String[] args) {
+        //declaracion de variables
+        int diasVacaciones;
+        String nombreMunicipio;
+        double iva;
+        boolean pidioDato;
+        // entrada de datos
+        diasVacaciones=25;
+        nombreMunicipio="Villa Nueva";
+        iva=350.02*0.12;
+        pidioDato=false;
+        //salida de datos
+        System.out.println("Cantidad de dias de vacaciones: "+diasVacaciones);
+        System.out.println("Nombre del municipio: "+nombreMunicipio);
+        System.out.println("el calculo del Iva es: "+iva);
+        System.out.println("se pidio datos al usuario: "+pidioDato);
+    }
+    
+}
